53ba28b147887f75ef307c5d670ac04fa9602713
Victor Furrer <vilefurrer@gmail.com>
2026-03-30 15:38:22 -0300

    Tag: novas paginas pro cadastro

--- bolsodireito/src/App.js
+++ bolsodireito/src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+//paginas-componentes do react
 import Login from "./componentes/Login";
 import Register from "./componentes/Register";
 import Dashboard from "./componentes/views/Dashboard";
 import Exemplos from "./Exemplos"
+import AddCategoria from "./componentes/cadastro/add_categoria";
+import AddGasto from "./componentes/cadastro/add_gasto"
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -13,6 +17,9 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/addcategoria" element={<AddCategoria />} />
+        <Route path="/addgasto" element={<AddCategoria />} />
+        <Route path="/nova-despesa" element={<AddCategoria />} />
       </Routes>
     </BrowserRouter>
     );
---
+++ bolsodireito/src/componentes/cadastro/add_gasto.js
@@ -0,0 +1,13 @@
+
+function AddGasto() {
+return (
+
+        <div> 
+            <h1> Qual foi o gasto? </h1>
+        </div>
+
+
+ //   <input type="number" placeholder="quanto money?"> 
+
+);
+}
